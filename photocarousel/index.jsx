cb9e09f402227c7caae53591c6fd470eb2c68aef
Shirley Wu <shirleyswwu@gmail.com>
2020-08-09 19:59:18 -0700

    broke out reactdom render in its own file

--- photocarousel/index.jsx
+++ photocarousel/index.jsx
@@ -1,25 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import axios from 'axios';
-// import DataElements from './components/DataElements.jsx';
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      // data: [],
-      // filteredData: []
-    }
-    // this.handleSubmit = this.handleSubmit.bind(this);
-    // this.newBalance = this.newBalance.bind(this);
-  }
-
-  render() {
-    return (
-      <div>i am react code</div>
-    )
-  }
-
-}
+import App from './App.jsx';
 
 ReactDOM.render(<App/>, document.getElementById('app'));
