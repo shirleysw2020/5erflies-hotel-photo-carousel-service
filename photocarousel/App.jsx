325905755084e3b8269deaa48ec37deaab164710
Shirley Wu <shirleyswwu@gmail.com>
2020-08-10 23:16:15 -0700

    refactored code

---
+++ photocarousel/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+import { useContext } from 'react';
+import axios from 'axios';
+import Header from './Header.jsx';
+import Gallery from './Gallery.jsx';
+import Button from './Button.jsx';
+
+// refactored class component to use hooks
+const App = (props) => {
+  // set state name and initial state
+  const [listing, setListing] = useState([]);
+  // console.log('clientb', listing);
+
+  useEffect(() => { // a hooks thing... similar to componentDidMount
+    axios.get('/listing?id=5')
+    .then((res) => {
+      // console.log('client:succes geting db', res.data);
+      setListing(res.data);
+    })
+    .catch((err) => {
+      console.log('client: failed getting db', err);
+    })
+  }, []);
+
+  if (listing.length == 0) {return null;}
+
+  return (
+    <div>
+      <MainWrapper>
+        <Header list={listing[0]}/>
+        <Gallery list={listing[0]}/>
+        <Button list={listing[0]}/>
+      </MainWrapper>
+    </div>
+  )
+}
+
+const theme = {
+  background: '#aaa',
+  color: '#24292e'
+};
+
+const MainWrapper = styled.div`
+  width: 60% ;
+  margin: 0 auto;
+  font-family: sans-serif, Roboto;
+`;
+
+export default App;
