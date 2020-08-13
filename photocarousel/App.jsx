6fb483f9662568d34e3d361a2264475eed6884ee
Shirley Wu <shirleyswwu@gmail.com>
2020-08-12 21:42:19 -0700

    render modal on state change

--- photocarousel/App.jsx
+++ photocarousel/App.jsx
@@ -5,13 +5,17 @@ import { useContext } from 'react';
 import axios from 'axios';
 import Header from './Header.jsx';
 import Gallery from './Gallery.jsx';
-import Button from './Button.jsx';
+// import Modal from './Modal.jsx';
 
 // refactored class component to use hooks
 const App = (props) => {
   // set state name and initial state
   const [listing, setListing] = useState([]);
-  const [showPopIp, setShow] = useState(false);
+  const [show, setShow] = useState(false);
+  // set show to true when user click open
+  const openModal = () => setShow(true);
+  // set show to false when user click close
+  const closeModal = () => setShow(false);
   // console.log('clientb', listing);
 
   useEffect(() => { // a hooks thing... similar to componentDidMount
@@ -29,13 +33,19 @@ const App = (props) => {
   }, []);
 
   if (listing.length == 0) {return null;}
-
+  if (show === true) {
+    return (<Modal>i am modal</Modal>)
+  }
   return (
     <div>
       <MainWrapper>
         <Header list={listing[0]}/>
-        <Gallery list={listing[0]}/>
-        <Button list={listing[0]}/>
+        <Gallery
+          list={listing[0]}
+          openModal={openModal}
+          closeModal={closeModal}
+          show={show}
+        />
       </MainWrapper>
     </div>
   )
@@ -47,9 +57,22 @@ const theme = {
 };
 
 const MainWrapper = styled.div`
-  width: 60% ;
+  width: 70% ;
   margin: 0 auto;
   font-family: sans-serif, Roboto;
 `;
 
+const Modal = styled.div`
+  width: 500px;
+  height: 100%;
+  position: relative;
+  margin: 0px auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 2px;
+  transform: translateY(100%);
+  transition: transform 0.2s ease;
+  box-shadow: 0 2px 8px 3px;
+  /* font-family: Helvetica, Arial, sans-serif; */
+`;
 export default App;
