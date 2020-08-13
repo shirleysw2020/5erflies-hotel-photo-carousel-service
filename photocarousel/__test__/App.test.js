a2aa1f2ee9859e9e6407c9b098bd71f5c3ee7fe1
Shirley Wu <shirleyswwu@gmail.com>
2020-08-13 01:01:01 -0700

    added tests

--- photocarousel/__test__/App.test.js
+++ photocarousel/__test__/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { expect } from 'chai';
 import App from '../App.jsx';
+import Header from '../Header.jsx';
+
 
 
 describe('First React component test with Enzyme', () => {
@@ -8,3 +11,4 @@ describe('First React component test with Enzyme', () => {
       shallow(<App />);
     });
 });
+
--- photocarousel/__test__/Button.test.js
+++ photocarousel/__test__/Button.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { create } from "react-test-renderer";
+import { shallow } from 'enzyme';
 import Button from '../Button.jsx';
 
 describe("Button component", () => {
@@ -8,3 +9,16 @@ describe("Button component", () => {
     expect(button.toJSON()).toMatchSnapshot();
   });
 });
+
+
+// const openModal = jest.fn();
+// describe('Button component', () => {
+//   it('button click should invoke function openModal', () => {
+//     const component = shallow(<Button onClick={openModal} />);
+//     component
+//       .find('sc-bwzfXH kVobZv')
+//       .simulate('click');
+//     expect(openModal).toHaveBeenCalled();
+//   });
+// });
+
